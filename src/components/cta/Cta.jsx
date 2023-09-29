0ff518b0cdfb237f116893954833aa22b2ff3040
christiannaira <christianmnaira@gmail.com>
2023-09-29 12:09:13 +0630

    designed the cta section, and added calendly

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -14,7 +14,10 @@ const Cta = () => {
 
                     <div className="cta-content">
 
+                        <h3>Need A High Performing Landing Page?</h3>
+                        <h2>Let's Get <span>Started!</span></h2>
 
+                        <a href="https://calendly.com/christiannaira/free-consultation" target='_blank' className='cta-button'>Book A Discovery Call</a>
 
                     </div>
 
